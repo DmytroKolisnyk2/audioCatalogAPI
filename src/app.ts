828ef0b021929b6b5a514b279ecba37cf3bf3dad
Max Popsuy <maxim09p@gmail.com>
2022-10-18 19:03:05 +0300

    #29: fixed cloudinary export

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import { appLogger, errorHandler, localizationMiddleware } from '@middlewares';
 import './utils/dotenv';
-import './utils/cloudinary-config';
 import './db/config';
 import { initRoutes } from '@routes';
 import { initServices } from '@services';
